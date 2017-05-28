f2927afa3b39079be90e0c83d3e0d23085a6e305
William Chavarria <wchavarria@tigo.com.gt>
2017-05-28 17:01:05 -0600

    punto 7 resuelto

--- voip.R
+++ voip.R
@@ -1,14 +1,18 @@
 #   ____________________________________________________________________________
 #   Usuarios VOIP                                                           ####
 
-      # Paquetesa a utilizar
-      pkgs <- c('data.table', 'tidyverse')
+      library(dplyr)
+      library(data.table)
+      library(scales)
       
-      # Cargar paquetes
-      inst <- lapply(pkgs, library, character.only = TRUE)
-      
+      # Leer una vez
       myurl <- 'http://talent.com.gt/IntroR/UsersVoIP.csv'
-      var_1 <- fread(myurl, data.table = FALSE)
+      var_1 <- as.tibble(fread(input = myurl,
+                               data.table = FALSE))
+      
+      # Guardar en formato binario
+      save(var_1, file = 'voip_users_raw_df.rda')
+      load('voip_users_raw_df.rda')
       
       
       # 2. Determine cuántos registros totales tiene el data frame resultante (cada registro representa información de un usuario)
@@ -16,14 +20,14 @@
       
       # 3. Determine cuántos usuarios son PREPAGO, cuátntos POSTPAGO y cuántos FACTURA FIJA (usar la variable CST_TYPE)
       var_3 <- var_1 %>%
-               group_by(CST_TYPE) %>%
-               summarise(cantidad_usuarios = n())
+               count(CST_TYPE, sort = TRUE) %>%
+               rename(cantidad_usuarios = 'n')
       
       # 4. Determine qué porcentaje de los usuarios totales son PREPAGO, POSTPAGO y FACTURA FIJA
       var_4 <- var_1 %>%
                group_by(CST_TYPE) %>%
                summarise(cantidad_usuarios = n()) %>%
-               mutate(porcentaje = cantidad_usuarios / sum(cantidad_usuarios))
+               mutate(porcentaje_usuarios_por_tipo_cliente = cantidad_usuarios / sum(cantidad_usuarios))
       
       # 5. Determine qué porcentaje del revenue total viene de usuarios PREPAGO, POSTPAGO y FACTURA FIJA (usar la variable REVENUE_TOTAL)
       var_5 <- var_1 %>%
@@ -35,12 +39,39 @@
       var_6 <- var_1 %>%
                group_by(CST_TYPE) %>%
                summarise(revenue = sum(REVENUE_INF)) %>%
-               mutate(porcentaje_revenue_broadband = revenue / sum(REVENUE_INF))
+               mutate(porcentaje_revenue_broadband = revenue / sum(revenue))
       
+      # Lazy approach      
+      # var_7 <- tibble(tipo_cliente = levels(as.factor(var_1$CST_TYPE)),
+      #                     cantidad_usuarios = var_4$cantidad_usuarios,
+      #                     porcentaje_usuarios_por_tipo = var_4$porcentaje_usuarios_por_tipo_cliente,
+      #                     porcentaje_revenue_total = var_5$porcentaje_revenue,
+      #                     porcentaje_revenue_broadband = var_6$porcentaje_revenue_broadband)
       
+      refcols <- c('CST_TYPE',
+                   'USERS',
+                   'USERS_PROPORTION',
+                   'REVENUE_PROPORTION',
+                   'REVENUE_INF_PROPORTION')
       
+      var_7 <-    var_1 %>%
+                  select(CST_TYPE, REVENUE_TOTAL, REVENUE_INF) %>%
+                  group_by(CST_TYPE) %>%
+                  summarise(USERS = n(),
+                            REVENUE = sum(REVENUE_TOTAL),
+                            REVENUE_BROADBAND = sum(REVENUE_INF)) %>%
+                  mutate(USERS_PROPORTION = percent(USERS / sum(USERS)),
+                         REVENUE_PROPORTION = percent(REVENUE / sum(REVENUE)),
+                         REVENUE_INF_PROPORTION = percent(REVENUE_BROADBAND  / sum(REVENUE_BROADBAND ))) %>%
+                  select(-REVENUE) %>%
+                  arrange(desc(USERS))
       
-      
+      var_7 <- var_7[refcols]
+                  
+             
+            
+            
+            
       
       
       
