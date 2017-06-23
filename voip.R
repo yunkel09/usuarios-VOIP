eeb7fb003666b9101881c61fe1f3437d7eba1f77
William Chavarria <wchavarria@tigo.com.gt>
2017-06-23 09:36:26 -0600

    server

--- voip.R
+++ voip.R
@@ -8,16 +8,16 @@
       list.of.packages <- c('data.table',  # funcion 'fread' lectura mas rapida que read_csv de readr
                             'dplyr',
                             'magrittr',    # compound assignment pipe operator %<>%   
-                            'scales')     # percent_format
-      
+                            'scales')      # percent_format
       new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
       if(length(new.packages)) install.packages(new.packages)
+      inst <- lapply(c(list.of.packages, new.packages), library, character.only = TRUE, quietly = TRUE)
 
-      lapply(c(list.of.packages, new.packages), library, character.only = TRUE, quietly = TRUE)
+      # definir opciones      
       options(scipen = 999)                # Evitar notacion cientifica
       
 ##  ............................................................................
-##  Step 1: Load Dataset                                                   ####
+##  Step 1: Load Dataset                                                    ####
 
       myurl <- 'http://talent.com.gt/IntroR/UsersVoIP.csv'
       var_1 <- as.tibble(fread(input = myurl,
@@ -36,74 +36,75 @@
 ##  Step 3: Calculations                                                    ####
 
             
+      # 3.1 caching grouping variables for speeding up the code
+      grp.v1      <- var_1 %>% group_by(cst_type)
+      
+      
       # 2. Registros totales
-      var_2 <-    nrow(var_1)
+      var_2       <-    nrow(var_1)
       
       # 3. Cantidad de usuarios por categoria
-      var_3 <-    var_1 %>%
-                  count(cst_type, sort = TRUE) %>%
-                  rename(users = n)
+      var_3       <-    var_1 %>%
+                        count(cst_type, sort = TRUE) %>%
+                        rename(users = n)
       
       # 4. Porcentaje de usuarios por categoria
-      var_4 <-    var_1 %>%
-                  group_by(cst_type) %>%
-                  summarise(users = n()) %>%
-                  mutate(user_proportion = percent(users / sum(users))) %>%
-                  arrange(desc(as.numeric(sub("%", "", user_proportion))))
+      var_4       <-    grp.v1 %>%
+                        summarise(users = n()) %>%
+                        mutate(user_proportion = percent(users / sum(users))) %>%
+                        arrange(desc(as.numeric(sub("%", "", user_proportion))))
       
-     
       # 5. Porcentaje de revenue por categoria de usuario
-      var_5 <-    var_1 %>%
-                  group_by(cst_type) %>%
-                  summarise(revenue = sum(revenue_total)) %>%
-                  mutate(revenue_proportion = percent(revenue / sum(revenue))) %>%
-                  arrange(desc(as.numeric(sub("%", "", revenue_proportion))))
+      var_5       <-    var_1 %>%
+                        group_by(cst_type) %>%
+                        summarise(revenue = sum(revenue_total)) %>%
+                        mutate(revenue_proportion = percent(revenue / sum(revenue))) %>%
+                  
+            
+                  # arrange(desc(as.numeric(sub("%", "", revenue_proportion)))) 
       
       # 6. Porcentaje de revenue broadband que por categoria de usuario
-      var_6 <-    var_1 %>%
-                  group_by(cst_type) %>%
-                  summarise(revenue = sum(revenue_inf)) %>%
-                  mutate(revenue_inf_proportion = percent(revenue / sum(revenue)))%>%
-                  arrange(desc(as.numeric(sub("%", "", revenue_inf_proportion))))
+      var_6       <-    var_1 %>%
+                        group_by(cst_type) %>%
+                        summarise(revenue = sum(revenue_inf)) %>%
+                        mutate(revenue_inf_proportion = percent(revenue / sum(revenue)))%>%
+                        arrange(desc(as.numeric(sub("%", "", revenue_inf_proportion))))
       
       # 7. Construir un df con los puntos anteriores
-      refcols <- c('cst_type',
-                   'users',                    # cantidad de usuarios
-                   'users_proportion',
-                   'revenue_proportion',
-                   'revenue_inf_proportion')
+      refcols     <-    c('cst_type',
+                         'users',                    # cantidad de usuarios
+                         'users_proportion',
+                         'revenue_proportion',
+                         'revenue_inf_proportion')
       
-      var_7 <-    var_1 %>%
-                  select(cst_type, revenue_total, revenue_inf) %>%
-                  group_by(cst_type) %>%
-                  summarise(users = n(),
-                            revenue = sum(revenue_total),
-                            revenue_broadband = sum(revenue_inf)) %>%
-                  mutate(users_proportion = percent(users / sum(users)),
-                         revenue_proportion = percent(revenue / sum(revenue)),
-                         revenue_inf_proportion = percent(revenue_broadband  / sum(revenue_broadband ))) %>%
-                  select(-revenue) %>%
-                  arrange(desc(users)) %>%
-                  do(.[refcols])
+      var_7       <-    var_1 %>%
+                        select(cst_type, revenue_total, revenue_inf) %>%
+                        group_by(cst_type) %>%
+                        summarise(users = n(),
+                                  revenue = sum(revenue_total),
+                                  revenue_broadband = sum(revenue_inf)) %>%
+                        mutate(users_proportion = percent(users / sum(users)),
+                               revenue_proportion = percent(revenue / sum(revenue)),
+                               revenue_inf_proportion = percent(revenue_broadband  / sum(revenue_broadband ))) %>%
+                        select(-revenue) %>%
+                        arrange(desc(users)) %>%
+                        do(.[refcols])
       
-      var_7 <-    var_7[refcols]                # reordenar columnas
-                  
       
       # 8. Porcentaje del revenue por tipo de dispositivo
-      var_8 <-    var_1 %>%
-                  select(revenue_inf, dvc_tp_nm) %>%
-                  group_by(dvc_tp_nm) %>%
-                  summarise(revenue_broadband = sum(revenue_inf)) %>%
-                  mutate(revenue_inf_proportion = percent(revenue_broadband / sum(revenue_broadband))) %>%
-                  arrange(desc(as.numeric(sub("%", "", revenue_inf_proportion))))
+      var_8       <-    var_1 %>%
+                        select(revenue_inf, dvc_tp_nm) %>%
+                        group_by(dvc_tp_nm) %>%
+                        summarise(revenue_broadband = sum(revenue_inf)) %>%
+                        mutate(revenue_inf_proportion = percent(revenue_broadband / sum(revenue_broadband))) %>%
+                        arrange(desc(as.numeric(sub("%", "", revenue_inf_proportion))))
             
       # 9. Agregar Revenue outgoing de cada usuario (REVENUE_TOTAL - REVENUE_INC_TOTAL)
-      var_1 <-    var_1 %>%
-                  mutate(revenue_outgoing = revenue_total - revenue_inc_total)
+      var_1       %<>%  mutate(revenue_outgoing = revenue_total - revenue_inc_total)
       
 
       # 10. ARPU outgoing total
-      var_10 <-   round(sum(var_1$revenue_outgoing)/n_distinct(var_1$msisdn_dd),2)
+      var_10      <-    round(sum(var_1$revenue_outgoing)/n_distinct(var_1$msisdn_dd),2)
       
       
       # 11. Determine el ARPU outgoing para los siguientes tipos de usuarios
@@ -112,62 +113,60 @@
       #' total. 
       
       # usuarios VOIP
-      var_11a <-  var_1 %>%
-                  select(revenue_outgoing, msisdn_dd, uservoip_max) %>%
-                  filter(uservoip_max == 1) %>%
-                  summarise(arpu_uservoip = sum(revenue_outgoing) / n_distinct(msisdn_dd)) %>%
-                  arrange(desc(arpu_uservoip)) %>%
-                  mutate_at('arpu_uservoip', round, digits = 2)
+      var_11a     <-    var_1 %>%
+                        select(revenue_outgoing, msisdn_dd, uservoip_max) %>%
+                        filter(uservoip_max == 1) %>%
+                        summarise(arpu_uservoip = sum(revenue_outgoing) / n_distinct(msisdn_dd)) %>%
+                        arrange(desc(arpu_uservoip)) %>%
+                        mutate_at('arpu_uservoip', round, digits = 2)
                               
       # usuarios whatsapp      
-      var_11b <-  var_1 %>%
-                  select(revenue_outgoing, msisdn_dd, whatsappvoip_max) %>%
-                  filter(whatsappvoip_max == 1) %>%
-                  summarise(arpu_whatsappvoip = sum(revenue_outgoing) / n_distinct(msisdn_dd)) %>%
-                  arrange(desc(arpu_whatsappvoip)) %>%
-                  mutate_at('arpu_whatsappvoip', round, digits = 2)
+      var_11b     <-    var_1 %>%
+                        select(revenue_outgoing, msisdn_dd, whatsappvoip_max) %>%
+                        filter(whatsappvoip_max == 1) %>%
+                        summarise(arpu_whatsappvoip = sum(revenue_outgoing) / n_distinct(msisdn_dd)) %>%
+                        arrange(desc(arpu_whatsappvoip)) %>%
+                        mutate_at('arpu_whatsappvoip', round, digits = 2)
                   
       # usuarios con trafico de datos, pero sin trafico VOIP
       # datauser_novoip
             
-      var_11c <-  var_1 %>%
-                  select(uservoip_max, msisdn_dd, mb_totales, voip_trffc_sum, revenue_outgoing) %>%
-                  filter(mb_totales > 0,
-                        voip_trffc_sum == 0) %>%
-                  summarise(arpu_datauser_novoip = round(sum(revenue_outgoing) / n_distinct(msisdn_dd), 2))
+      var_11c     <-    var_1 %>%
+                        select(uservoip_max, msisdn_dd, mb_totales, voip_trffc_sum, revenue_outgoing) %>%
+                        filter(mb_totales > 0,
+                              voip_trffc_sum == 0) %>%
+                        summarise(arpu_datauser_novoip = round(sum(revenue_outgoing) / n_distinct(msisdn_dd), 2))
                         
-      
       # usuarios sin trafico de datos
-      var_11d <-  var_1 %>%
-                  filter(mb_totales == 0) %>%
-                  summarise(arpu_users_with_no_data_trffc = round(sum(revenue_outgoing) / n_distinct(msisdn_dd), 2))
+      var_11d     <-    var_1 %>%
+                        filter(mb_totales == 0) %>%
+                        summarise(arpu_users_with_no_data_trffc = round(sum(revenue_outgoing) / n_distinct(msisdn_dd), 2))
                         
-            
       # usuarios con trafico entrante internacional      
-      var_11e <-  var_1 %>%
-                  filter(user_inc_intl > 0) %>%
-                  summarise(arpu_users_with_intl_trffc = round(sum(revenue_outgoing) / n_distinct(msisdn_dd), 2))
+      var_11e     <-    var_1 %>%
+                        filter(user_inc_intl > 0) %>%
+                        summarise(arpu_users_with_intl_trffc = round(sum(revenue_outgoing) / n_distinct(msisdn_dd), 2))
       
       
-      var_11 <-   as.data.frame(list(var_11a, var_11b, var_11c, var_11d, var_11e)) %>%
-                  gather(key = 'tipo', value = 'arpu_qtz') %>%
-                  mutate_at(.vars = 'tipo', .funs = str_replace_all, 'arpu_', "") %>%
-                  arrange(desc(arpu_qtz))
+      var_11      <-    as.data.frame(list(var_11a, var_11b, var_11c, var_11d, var_11e)) %>%
+                        gather(key = 'tipo', value = 'arpu_qtz') %>%
+                        mutate_at(.vars = 'tipo', .funs = str_replace_all, 'arpu_', "") %>%
+                        arrange(desc(arpu_qtz))
                   
       
 
       # 12. ARPU outgoing por tipo de dispositivo
-      var_12 <-   var_1 %>%
-                  group_by(dvc_tp_nm) %>%
-                  summarise(arpu_by_dvc_tp = sum(revenue_outgoing) / n_distinct(msisdn_dd)) %>%
-                  arrange(desc(arpu_by_dvc_tp)) %>%
-                  mutate_at('arpu_by_dvc_tp', round, digits = 2)
+      var_12      <-    var_1 %>%
+                        group_by(dvc_tp_nm) %>%
+                        summarise(arpu_by_dvc_tp = sum(revenue_outgoing) / n_distinct(msisdn_dd)) %>%
+                        arrange(desc(arpu_by_dvc_tp)) %>%
+                        mutate_at('arpu_by_dvc_tp', round, digits = 2)
       
 
 ##  ............................................................................
 ##  Guardar workspace                                                       ####
 
-      rm(var_1, pkgs)
+      rm(var_1, pkgs, inst, new.packages, list.of.packages, grp.v1)
       save.image(file = 'workspace_ejercicio_1.RData')
             
             
